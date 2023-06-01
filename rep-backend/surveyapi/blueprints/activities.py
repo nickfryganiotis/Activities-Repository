7d1021cffbe99a2660f8ee47ec0b0f8210d0ca1d
nickfryganiotis <nikolasfryganiotis@yahoo.com>
2023-06-02 02:49:25 +0300

    Filter activities almost finished, missing pagination

--- rep-backend/surveyapi/blueprints/activities.py
+++ rep-backend/surveyapi/blueprints/activities.py
@@ -150,50 +150,82 @@ def test():
         print(activity.activity_translations[0].to_dict())
         return "hi"
 
-@activities.route('/test2/', methods=('GET',))
-def test2():
-    if request.method=='GET':
+@activities.route('/test2/<string:language_code>/', methods=('POST',))
+def test2(language_code):
+    if request.method=='POST':
         
-        competences = ["Emotional regulation", "Flexibility"]
-        didactic_strategies = ['Collaborative learning', 'Case study learning']
+        data = request.get_json()
 
-        subquery_1 = db.session.\
-            query(Activity_competence.activity_id).\
-                join(Competence).\
-                    filter(Competence.code.in_(competences)).\
-                        group_by(Activity_competence.activity_id).\
-                            subquery()
-        subquery_2 = db.session.\
-            query(Activity_didactic_strategy.activity_id).\
-                join(Didactic_strategy).\
-                    filter(Didactic_strategy.code.in_(didactic_strategies)).\
-                        group_by(Activity_didactic_strategy.activity_id).\
-                            subquery()
-                            
-        subquery_12 = db.session.\
-                        query(subquery_1.c.activity_id).\
-                            join(subquery_2, subquery_1.c.activity_id==subquery_2.c.activity_id).\
-                                group_by(subquery_1.c.activity_id).subquery()
-        # print(subquery_12)
+        ## Set a list variable for the query conjuction parameters concerining of activity and activity_translation
+
+        query_parameters = []   
+
+        ## Activity attributes
         
-        # query = Activity.query.\
-        #         join(subquery_12).all()
+        query_parameters.append(or_(*[Activity.periodicity == periodicity for periodicity in data['periodicity']]))     
+        query_parameters.append(or_(*[and_(Activity.min_age == min_age, Activity.max_age == max_age)
+                                       for (min_age, max_age) in [tuple([int(x) for x in age_target_group_case])
+                                                        for age_target_group_case in [age_target_group.split('-')
+                                                                for age_target_group in data['age_target_group']]]]))
+        query_parameters.append(or_(*[Activity.duration == duration_to_num(duration) for duration in data['duration']]))
+        query_parameters.append(or_(*[Activity.sub_grouping == sub_grouping_to_num(sub_grouping) 
+                                   for sub_grouping in data['sub_grouping']]))
+        if not data['teacher_role'] == "":
+            query_parameters.append(Activity.teacher_role == data['teacher_role'])
+        
+        ## Activity_translation attributes
+
+        if not data['title'] == "":
+            query_parameters.append(Activity_translation.title == data['title'])
+        query_parameters.append(Activity_translation.language_code == language_code)
                        
         activities = Activity.\
                         query.\
                             join(Activity_translation).\
-                                filter_by(language_code='en')
+                                filter(and_(*query_parameters))
                                     
+        ## Competences, Didactic strategies, Special needs
+        
+        competences = data['competences']
+        didactic_strategies = data['didactic_strategies']
+        special_needs = data['special_needs']
+
+        activity_competences = db.session.\
+                                query(Activity_competence.activity_id).\
+                                    join(Competence).\
+                                        filter(Competence.code.in_(competences)).\
+                                            group_by(Activity_competence.activity_id).\
+                                                subquery()
+                
+        activity_didactic_strategies = db.session.\
+                                        query(Activity_didactic_strategy.activity_id).\
+                                            join(Didactic_strategy).\
+                                                filter(Didactic_strategy.code.in_(didactic_strategies)).\
+                                                    group_by(Activity_didactic_strategy.activity_id).\
+                                                        subquery()
+        
+        activity_special_needs = db.session.\
+                                    query(Activity_special_need.activity_id).\
+                                        join(Special_need).\
+                                            filter(Special_need.code.in_(special_needs)).\
+                                                group_by(Activity_special_need.activity_id).\
+                                                    subquery()
+        
         if competences:
-            activities = activities.join(subquery_1)
+            activities = activities.join(activity_competences)
         
         if didactic_strategies:
-            activities = activities.join(subquery_2)
+            activities = activities.join(activity_didactic_strategies)
+        
+        if special_needs:
+            activities = activities.join(activity_special_needs)
+
+        ## uncomment to print query
+        # print(activities)    
         
         activities = activities.all()
 
-        print(activities)
-        return "hi"
+        return {"activities": [activity.to_dict() for activity in activities]}
     else:
         return "Error"     
 
