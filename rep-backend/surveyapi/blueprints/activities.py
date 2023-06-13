3f1a1d5ad21a37ae33aabfab65a49e29546fa530
nickfryganiotis <nikolasfryganiotis@yahoo.com>
2023-06-13 20:01:31 +0300

    add token_required function, but there is a bug

--- rep-backend/surveyapi/blueprints/activities.py
+++ rep-backend/surveyapi/blueprints/activities.py
@@ -10,21 +10,64 @@
 
 from flask import request
 from models import db, Activity, Activity_competence, Activity_translation, Competence, Didactic_strategy, Activity_didactic_strategy, Special_need, Activity_special_need, User
-from flask import Blueprint, jsonify
+from flask import Blueprint, jsonify, current_app
 from sqlalchemy import or_, and_
 from helpers import duration_to_num, sub_grouping_to_num
+from functools import wraps
+import jwt
 #app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
 
 activities = Blueprint('activities', __name__)
 
+"""
+token_required
+- Function that manage the token received by the call.
+"""
+def token_required(f):
+    @wraps(f)
+    def _verify(*args, **kwargs):
+        auth_headers = request.headers.get('Authorization', '').split()
+
+        invalid_msg = {
+            'message': 'Invalid token. Registration and / or authentication required',
+            'authenticated': False
+        }
+        expired_msg = {
+            'message': 'Expired token. Reauthentication required.',
+            'authenticated': False
+        }
+
+        if len(auth_headers) != 2:
+            return jsonify(invalid_msg), 401
+        try:
+            token = auth_headers[1]
+            # data = jwt.decode(token, current_app.config['SECRET_KEY'])
+            data = jwt.decode(jwt=token,
+            key=current_app.config['SECRET_KEY'],
+            algorithms=["HS256"])
+            user = User.query.filter_by(email=data['sub']).first()
+            #print(user)
+            if not user:
+                raise RuntimeError('User not found')
+            return f(user, *args, **kwargs)
+        except jwt.ExpiredSignatureError:
+            return jsonify(expired_msg), 401 # 401 is Unauthorized HTTP status code
+        except (jwt.InvalidTokenError, Exception) as e:
+            print(e)
+            return jsonify(invalid_msg), 401
+
+    return _verify
+
+
 @activities.route('/create_activity/', methods=('POST',))
-def create_activity():
+@token_required
+def create_activity(current_user):
     if request.method=='POST':
         data = request.get_json()
 
         ## add activity attributes
 
-        new_activity = Activity(**data['activity'])        
+        new_activity = Activity(**data['activity'], creator=current_user.id)        
 
         db.session.add(new_activity)
         db.session.commit()
